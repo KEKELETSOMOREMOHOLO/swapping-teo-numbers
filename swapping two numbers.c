c708ff5576114788aafe728d2cc407d4e4766ce5
KEKELETSOMOREMOHOLO <118248938+KEKELETSOMOREMOHOLO@users.noreply.github.com>
2023-02-18 13:23:20 +0530

    Create swapping two numbers.c

---
+++ swapping two numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+  int first, second, temp;
+  
+  printf("Enter the first number:");
+  scanf("%d",&first);
+  printf("Enter the second number:");
+  scanf("%d",&second);
+  
+  temp = first;
+  first = second;
+  second = temp;
+  
+  printf("\nAfter swapping, forst number = %d \n",first);
+  printf("After swapping, the second number = %d",second);
+  
+  return 0;
+}
